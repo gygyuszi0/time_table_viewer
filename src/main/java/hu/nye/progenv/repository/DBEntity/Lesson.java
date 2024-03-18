e6b334f1ec77d8e7e6d4f0ec04911bb753613677
gygyuszi0 <gyorgyigyula8@gmail.com>
2024-03-18 17:41:40 +0100

    Lesson entity és függőségek

---
+++ src/main/java/hu/nye/progenv/repository/DBEntity/Lesson.java
@@ -0,0 +1,25 @@
+package hu.nye.progenv.repository.DBEntity;
+
+import hu.nye.progenv.controller.model.LessonTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Lesson {
+
+    @Id
+    private Long id;
+    private String name;
+    private LessonTime startTime;
+    private LessonTime stopTime;
+    private String room;
+
+}
