db49b520dc7b1d539f049d540357945462a5c72d
gygyuszi0 <gyorgyigyula8@gmail.com>
2024-03-19 17:14:49 +0100

    Control lesson mentésére

--- src/main/java/hu/nye/progenv/controller/LessonControl.java
+++ src/main/java/hu/nye/progenv/controller/LessonControl.java
@@ -1,4 +1,21 @@
 package hu.nye.progenv.controller;
 
-public interface LessonControl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.nye.progenv.service.LessonInterface;
+import hu.nye.progenv.service.model.LessonRequest;
+import hu.nye.progenv.service.model.LessonResponse;
+
+@RestController
+public class LessonControl {
+
+    @Autowired
+    private LessonInterface lessonService;
+
+    @PostMapping("/create_lesson")    
+    public LessonResponse createLesson(LessonRequest lesson) {
+        return lessonService.createLesson(lesson);
+    }
 }
